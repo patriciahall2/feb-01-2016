53a2653952995e5b793488aa874487013ae726e9
Elizabeth Banach and Patricia Hall <student@epicodus.com>
2016-02-01 20:52:29 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you - THIS IS A HEADER!")
+  });
+
+  $("p").dblclick(function() {
+    alert("This is a paragraph.")
+  });
+
+  $("img").dblclick(function() {
+    alert("This is Eva, a Siamese cat.")
+  });
+
+  $("h2").click(function() {
+    alert("This is header 2.")
+  });
+
+});
